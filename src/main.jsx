fad200399369db09002b03a87f5bdf080199a15b
Deepti Lodhi <lodhideepti39@gmail.com>
2025-05-10 10:27:48 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './style.css'
-import { Fragment } from 'react'
-import { ToastContainer } from 'react-toastify'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './style.css';
+import { Fragment } from 'react';
+import { ToastContainer } from 'react-toastify';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-
-  <Fragment>
-    <App />
-    <ToastContainer />
-  </Fragment>
-)
+  <BrowserRouter basename="/react project">
+    <Fragment>
+      <App />
+      <ToastContainer />
+    </Fragment>
+  </BrowserRouter>
+);
